9c36bc67d96db4d54131300183a9666564af1b1c
tian.hu <tian.hu@alibaba-inc.com>
2017-01-25 11:45:16 +0800

    add config_file command

--- Tutorial/main.cpp
+++ Tutorial/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "config.h"
 
 using namespace std;
 
 int main()
 {
+	cout<<"Tutorial_VERSION_MAJOR: "<<Tutorial_VERSION_MAJOR<<endl;
+    cout<<"Tutorial_VERSION_MINOR: "<<Tutorial_VERSION_MINOR<<endl;
 	cout<<"hello world"<<endl;
 }
